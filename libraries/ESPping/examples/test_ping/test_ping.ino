1b638173b3a8085e2c64ff84b82d7ac2907c1d9e
Anudeep Chandra Paul <anudeepch.paul@gmail.com>
2023-02-25 10:05:49 +0530

    [Initial Commit] - First commit

---
+++ libraries/ESPping/examples/test_ping/test_ping.ino
@@ -0,0 +1,54 @@
+/******************************************************
+  test Ping
+******************************************************/
+
+#include <ESPping.h>
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(100);
+
+  WiFi.begin("ssid","password");
+  
+  // attente connexion
+  Serial.print("\nConnection");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("\nConnection OK, IP: ");
+  Serial.print(WiFi.localIP());
+  Serial.print(" Gateway: ");
+  Serial.println(WiFi.gatewayIP());
+  Serial.println("You can try to ping me ;-)");
+
+}
+
+void loop()
+{
+  
+  // Ping IP
+  const IPAddress remote_ip(9,9,9,9);
+  Serial.print(remote_ip);
+  if (Ping.ping(remote_ip)){
+      Serial.printf(" response time : %d/%.2f/%d", Ping.minTime(), Ping.averageTime(), Ping.maxTime());
+      Serial.println("ms");
+  } else {
+    Serial.println(" Error !");
+  }
+  delay(5000);
+
+  // Ping Host
+  const char* remote_host = "quad9.net";
+  Serial.print(remote_host);
+  if (Ping.ping(remote_host)){
+      Serial.printf(" response time : %d/%.2f/%d", Ping.minTime(), Ping.averageTime(), Ping.maxTime());
+      Serial.println("ms");
+  } else {
+    Serial.println(" Ping Error !");
+  }    
+  delay(5000);
+
+}
